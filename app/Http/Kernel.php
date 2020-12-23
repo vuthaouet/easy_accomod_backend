ce050dc6b662c807fc6c1909dc8889543049192f
vuthaouet <vuthao.ak56ht@gmail.com>
2020-12-23 16:57:51 +0700

    some fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
+        'cors' => \Barryvdh\Cors\Middleware\HandleCors::class,
     ];
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class cors
+class Cors
 {
     /**
      * Handle an incoming request.
